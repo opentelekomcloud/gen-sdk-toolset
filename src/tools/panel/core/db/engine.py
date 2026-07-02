1ac32bb34c03690d9bbc552c4c266e28002d9faf
Valeriia <sand16031986@gmail.com>
2026-07-02 12:12:24 +0200

    :sparkles: postgres-sqlite event

--- src/tools/panel/core/db/engine.py
+++ src/tools/panel/core/db/engine.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import Engine
 
@@ -9,3 +9,10 @@ engine = create_engine(settings.database.url)
 SessionLocal = sessionmaker(bind=engine)
 
 
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    """Enable FK enforcement on SQLite (off by default; Postgres ignores this)."""
+    if engine.dialect.name == "sqlite":
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
