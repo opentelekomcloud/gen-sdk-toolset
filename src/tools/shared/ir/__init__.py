afd5510a240e3df7a46ac22cfba63289559be3bf
Valeriia <sand16031986@gmail.com>
2026-07-23 15:56:18 +0200

    ✨ feat: version the serialized document contract in shared ir

--- src/tools/shared/ir/__init__.py
+++ src/tools/shared/ir/__init__.py
@@ -7,7 +7,12 @@ from .repository import Repository
 from .section import Section, SectionName
 from .service import Service
 
+#: Version of the serialized Document/Endpoint contract. Bump on any breaking
+#: change to the IR models so persisted payloads can be told apart.
+DOCUMENT_SCHEMA_VERSION = "1"
+
 __all__ = [
+    "DOCUMENT_SCHEMA_VERSION",
     "HttpMethod",
     "ParameterType",
     "Document",
